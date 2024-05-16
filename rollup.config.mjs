35e0f9d3e4db0c52526758330b7dc022a5caa03e
Mohsen <sm.shafaei@gmail.com>
2024-05-16 14:10:56 +0300

    adding css to the button

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,8 @@ import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
 
+import postcss from "rollup-plugin-postcss"
+
 
 export default [
     {
@@ -25,6 +27,9 @@ export default [
             resolve(),
             commonjs(),
             typescript({tsconfig: "./tsconfig.json"}),
+            postcss({
+                plugins:[]
+              })
 
         ],
     },
@@ -32,6 +37,7 @@ export default [
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.(css|less|scss)$/],
     },
 
 ];
