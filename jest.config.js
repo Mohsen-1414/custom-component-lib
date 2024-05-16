6348fd7f35a727669c12e4dfd56f548f4217437b
Mohsen <sm.shafaei@gmail.com>
2024-05-16 15:48:13 +0300

    updated version of Button component

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-      '^.+\\.tsx?$': 'babel-jest'
-    }
-  };
-  
